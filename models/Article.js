b3bdb045151ba80a6f571d44dd0c114a5dbfe955
Luke <kainoa59@gmail.com>
2019-04-22 09:28:06 -0700

    Can't append delete/save button to each article, instead there is only one button

---
+++ models/Article.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema({
+    title: {
+        type: String,
+    },
+    link: {
+        type: String,
+    },
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+});
+
+const Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const db = require("./models")
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/webscraper", {useNewUrlParser: true, useCreateIndex: true});
+
+app.get("/scrape", (req, res) => {
+    axios.get("http://nyt-mongo-scraper.herokuapp.com/").then(response => {
+        const $ = cheerio.load(response.data);
+        $(".card").each(function (i, element) {
+            const result = {};
+            result.title = $(this).children(".card-header").children("h3").text();
+            result.link = $(this).children(".card-header").children("h3").attr("href");
+
+            db.Article.create(result)
+            .then(dbArticle => {
+                console.log(dbArticle);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        });
+        res.send("Scrape Complete");
+    });
+});
+
+app.get("/articles", (req, res) => {
+    db.Article.find({})
+      .then(dbArticle => {
+        res.json(dbArticle);
+      }).catch(err => {
+        res.json(err);
+      });
+  });
+
+app.get("/articles/:id", (req, res) => {
+    db.Article
+        .findOne({ _id: req.params.id })
+        .populate("note")
+        .then(dbArticle => {
+            res.json(dbArticle);
+        }).catch(err => {
+            res.json(err);
+        });
+});
+
+app.post("/articles/:id", (req, res) => {
+    db.Note.create(req.body)
+    .then(dbNote => {
+        return db.Article.findOneAndUpdate({_id: req.params.id}, {note: dbNote._id}, {new: true});
+    }).then(dbArticle => {
+        res.json(dbArticle);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`)
+});
